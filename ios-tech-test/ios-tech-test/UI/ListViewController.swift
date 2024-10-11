986c0ebc045151086d3d6305967b24608d35d88b
Tarek <jradi3@gmail.com>
2024-10-11 12:05:15 +0100

    Candidate repo initialization

---
+++ ios-tech-test/ios-tech-test/UI/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewController.swift
+//  ios-tech-test
+//
+//  Created by Tarek Jradi on 11/10/2024.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
